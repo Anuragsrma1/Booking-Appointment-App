0d398db681f81532a11ef5f3986e27e95e05e733
anuragsrma <anuragsharma0140@gmail.com>
2023-11-17 09:01:35 +0530

    delete user

--- index.js
+++ index.js
@@ -20,7 +20,7 @@
       birthdate,
       gender
    }     
-       axios.post("https://crudcrud.com/api/0059f115b1064e16af70a175445f0e99/appointmentdata",obj)
+       axios.post("https://crudcrud.com/api/d3a2075881d24d49973679688c165e6a/appointmentdata",obj)
             .then((response) => {
                showUserOnScreen(response.data);
                  console.log(response);
@@ -35,49 +35,58 @@
 
   window.addEventListener("DOMContentLoaded", ()=> {
         
-     axios.get("https://crudcrud.com/api/0059f115b1064e16af70a175445f0e99/appointmentdata")
+   
+     axios.get("https://crudcrud.com/api/d3a2075881d24d49973679688c165e6a/appointmentdata")
          
      .then((response) => {
-
         console.log(response);
 
         for(var i=0; i<response.data.length; i++ ){
          showUserOnScreen(response.data[i]);
         } 
    })
-   .catch((err) => {
-      console.log(err);
+       .catch((err) => {
+          console.log(err);
    })
   })
 
+   function showUserOnScreen(user){
 
 
-   function showUserOnScreen(obj){
     const parentElem = document.getElementById('listOfItems')
             
     const childElem = document.createElement('li')
-   childElem.textContent = obj.name + '-' + obj.email + '-' + obj.password + '-' + obj.birthdate + '-' + obj.gender 
-
+   childElem.textContent = user.name + '-' + user.email + '-' + user.password + '-' + user.birthdate + '-' + user.gender 
+  //    console.log(user._id);
    const deleteButton = document.createElement('input')    
    deleteButton.type = "button"
    deleteButton.value = 'Delete'
    deleteButton.onclick = () => {
-      localStorage.removeItem(obj.email)
+    //  localStorage.removeItem(user.email)
+    axios.delete(`https://crudcrud.com/api/d3a2075881d24d49973679688c165e6a/appointmentdata/6556dcd8f3272103e862cbc4`)
+    .then((response) => {
+        console.log(response)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
       parentElem.removeChild(childElem)
    }
+   
    
    const editButton = document.createElement('input')
    editButton.type = "button"
    editButton.value = 'Edit'
     editButton.onClick = () => {
-      localStorage.removeItem(obj.email)
+      localStorage.removeItem(user.email)
       parentElem.removeChild(childElem)
-   document.getElementById('name').value = obj.fullname
-   document.getElementById('email').value = obj.email 
-   document.getElementById('password').value = obj.password
-   document.getElementById('birthdate').value = obj.birthdate
-   document.getElementById('gender').value = obj.gender 
-   document.get
+   document.getElementById('name').value = user.fullname
+   document.getElementById('email').value = user.email 
+   document.getElementById('password').value = user.password
+   document.getElementById('password').value = user.password
+   document.getElementById('birthdate').value = user.birthdate
+   document.getElementById('gender').value = user .gender 
+   
    }  
 
    childElem.appendChild(deleteButton)
@@ -86,7 +95,3 @@
 
 }
  
-function addNums(num1,nums2){
-   return num1 + nums2;
-}
- console.log(addNums(5,6));
